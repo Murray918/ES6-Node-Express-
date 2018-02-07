29cce619f9996073aacda5794b46d42cf30a6351
Andrew Murray <darbyguitars@gmail.com>
2018-02-07 02:08:08 -0600

    ES6 hello world

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,22 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+app.use((0, _cors2.default)());
+
+app.get('/', function (req, res) {
+	res.send({ message: 'there is a snake in your boot' });
+});
 
 app.listen(3000, function () {
 	console.log('party port is open for buisness');
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send({ message: 'there is a snake in your boot' });
+});
 
 app.listen(3000, () => {
 	console.log('party port is open for buisness');
